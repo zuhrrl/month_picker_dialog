31cfc0912496e5f42ed0ad246bbf3fc655dd24e7
Zuhrul Anam <zuhrrlanam@gmail.com>
2024-07-24 15:24:22 +0700

    update to flutter latest and intl

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\HP\Downloads\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\SP\flutterProj\sample_projs\forked_repos\month_picker_dialog\example"
+export "FLUTTER_ROOT=/Users/wdt/dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/wdt/Documents/zul/month_picker_dialog/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
